21ea3d22ac396f8849e559e1bbced14dcb547015
Nitish-Mahant <138679586+Nitish-Mahant@users.noreply.github.com>
2024-08-19 14:22:59 +0530

    Add files via upload

---
+++ Project Training Amdocs/src/Part2/Fourth.java
@@ -0,0 +1,106 @@
+package Part2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class Fourth {
+	@Test
+    public void sample0()
+    {
+        // Set up the WebDriver and open the browser
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\Downloads\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        try
+        {
+            driver.manage().window().maximize();
+            driver.get("https://www.selenium.dev/");
+            WebElement link = driver.findElement(By.linkText("Downloads"));
+            link.click();
+            Thread.sleep(2000);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            driver.quit();
+        }
+    }
+    @Test
+    public void OpeiningFile()
+    {
+        // Set up the WebDriver and open the browser
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\Downloads\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        try
+        {
+            driver.manage().window().maximize();
+            driver.get("https://www.selenium.dev/");
+            WebElement link = driver.findElement(By.linkText("Projects"));
+            link.click();
+            Thread.sleep(2000);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            driver.quit();
+        }
+    }
+    @Test
+    public void FindByLink()
+    {
+        // Set up the WebDriver and open the browser
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\Downloads\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        try
+        {
+            driver.manage().window().maximize();
+            driver.get("https://www.selenium.dev/");
+            WebElement link = driver.findElement(By.linkText("Documentation"));
+            link.click();
+            Thread.sleep(2000);
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            driver.quit();
+        }
+    }
+    @Test
+    public void Search()
+    {
+        // Set up the WebDriver and open the browser
+    	System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\Downloads\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        try
+        {
+            driver.manage().window().maximize();
+            driver.get("https://www.selenium.dev/");
+            WebElement link = driver.findElement(By.linkText("Documentation"));
+            link.click();
+
+            WebElement search1 = driver.findElement(By.className("DocSearch-Button-Placeholder"));
+            search1.click();
+            WebElement search2 = driver.findElement(By.className("DocSearch-Input"));
+            search2.sendKeys("Selenium IDE");
+            search2.click();
+            Actions builder = new Actions(driver);
+            builder.moveToElement(search2).sendKeys(Keys.DOWN).sendKeys(Keys.ENTER);
+            Thread.sleep(2000);
+            driver.quit();
+
+        }
+        
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            driver.quit();
+        }
+    }
+}
+
